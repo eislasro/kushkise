0ff978c4aa3a5802b210e989b7dd250b4afd3296
eislasro <132082710+eislasro@users.noreply.github.com>
2023-05-29 13:30:11 -0600

    Update LoadCheckout.js
    modificación de tarjeta enmascarada

--- js/LoadCheckout.js
+++ js/LoadCheckout.js
@@ -258,7 +258,7 @@ function validarTarjeta(cardNumber) {
       currency: '${currency}',
       card: {
           name: '${$('#credit-card-name').val()}',
-          number: '${masked}',
+          number: '${$('#credit-card-number').val(masked)}',
           cvc: '***',
           expiryMonth:  '${$('#card-date').val().split('/')[0]}',
           expiryYear: '${$('#card-date').val().split('/')[1]}',
